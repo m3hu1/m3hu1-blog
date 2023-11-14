74aca9e8ef807856d75e78352f5f992133509d29
Mehul Pathak <pathakkmehul@gmail.com>
2023-11-14 19:32:28 +0530

    updated interface.ts

--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -1,10 +1,10 @@
 export interface Post {
-    title: string;
-    overview: string;
-    content: any;
-    _id: string;
-    slug: {
-      current: string;
-    };
-    _createdAt: string;
-  }
+  title: string;
+  overview: string;
+  content: any;
+  _id: string;
+  slug: {
+    current: string;
+  };
+  _createdAt: string;
+}
